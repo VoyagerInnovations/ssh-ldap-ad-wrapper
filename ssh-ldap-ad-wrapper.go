b1f08b83387f07a3a2d9563fb6edf1fad9eec59d
John Alvero <johnhomer@gmail.com>
2018-03-21 15:26:37 +0800

    update ssh-ldap-wrapper.go
    added support for multiple servers with fail-over
    removed the need to have separate ssh-ldap.conf
    now only uses /etc/nslcd.conf

--- ssh-ldap-ad-wrapper.go
+++ ssh-ldap-ad-wrapper.go
@@ -1,36 +1,33 @@
 package main
 
 import (
-
+	"os"
         "log"
         "fmt"
         "gopkg.in/ldap.v2"
-        "os"
         "crypto/tls"
         "crypto/x509"
         "io/ioutil" 
         "bufio"
         "strings"
-        "strconv"
-
 )
 
-var ldap_config_file = "/etc/ssh-ldap-ad.conf"
+var ldap_config_file = "/etc/nslcd.conf"
+var pubkey_property = "altSecurityIdentities"
 
 func main() {
 
         if len(os.Args) > 1  {
-        
-                var uri string
-                var hostname string
-                var port int64
+
+		var l  *ldap.Conn
+		var conf *tls.Config
+       		var uname = os.Args[1] 
                 var user string
                 var pass string
                 var base_search string
-                var pubkey_property string
                 var server_rootca string
-                var uname = os.Args[1]
-
+		var uris [2]string
+		var uris_cnt int64
 
                 file, err := os.Open(ldap_config_file)
                 if err != nil {
@@ -43,59 +40,44 @@ func main() {
                         
                         if (!strings.HasPrefix(scanner.Text(),"#")) {
 
-                                if (strings.HasPrefix(scanner.Text(), "ip")) {
-                                        s := strings.Split(scanner.Text(), " ") 
-                                        uri = s[1]
-                                        continue 
+                                if (strings.HasPrefix(scanner.Text(), "uri")) {
+                                        s := strings.Split(scanner.Text(), " ")
+					ss := strings.Split(s[1],"//")
+                                        uris[uris_cnt] = ss[1]
+					uris_cnt++
+                                        continue
                                 }
 
-                                if (strings.HasPrefix(scanner.Text(), "port")) {
-                                        s := strings.Split(scanner.Text(), " ") 
-                                        p := s[1]
-                                        port, err = strconv.ParseInt(p, 10, 32)
-                                        continue 
-                                }
-
-                                if (strings.HasPrefix(scanner.Text(), "hostname")) {
-                                        s := strings.Split(scanner.Text(), " ") 
-                                        hostname = s[1]
-                                        continue 
-                                }
-
-                                if (strings.HasPrefix(scanner.Text(), "base_search")) {
+                                if (strings.HasPrefix(scanner.Text(), "base")) {
                                         s := strings.Split(scanner.Text(), " ") 
                                         base_search = s[1]
                                         continue 
                                 }
+
                                 if (strings.HasPrefix(scanner.Text(), "binddn")) {
                                         s := strings.Split(scanner.Text(), " ") 
                                         user = s[1]
                                         continue 
                                 }
+
                                 if (strings.HasPrefix(scanner.Text(), "bindpw")) {
                                         s := strings.Split(scanner.Text(), " ") 
                                         pass = s[1]
                                         continue 
                                 }
 
-                                if (strings.HasPrefix(scanner.Text(), "pubkey_property")) {
-                                        s := strings.Split(scanner.Text(), " ") 
-                                        pubkey_property = s[1]
-                                        continue 
-                                }
-                                
-                                if (strings.HasPrefix(scanner.Text(), "server_rootca")) {
+                                if (strings.HasPrefix(scanner.Text(), "tls_cacertfile")) {
                                         s := strings.Split(scanner.Text(), " ") 
                                         server_rootca = s[1]
                                         continue 
                                 }
-
                         }
-        }
-        
+        	}
+ 
                 if err := scanner.Err(); err != nil {
-                log.Fatal(err)
+                	log.Fatal(err)
                 }
+
 
                 //Load CA Cert
 
@@ -106,17 +88,26 @@ func main() {
                 certPool := x509.NewCertPool()
                 certPool.AppendCertsFromPEM(cert)
 
-                conf := &tls.Config{
-                        RootCAs: certPool,
-                        InsecureSkipVerify: false,
-                        ServerName: hostname,
-                }
-
                 // Connect
-                l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", uri, port))
-                if err != nil {
+
+		for _, uri := range uris {
+			ss := strings.Split(uri,":")
+                	conf = &tls.Config{
+                        	RootCAs: certPool,
+                        	InsecureSkipVerify: false,
+                        	ServerName: ss[0],
+                	}
+
+			l, err = ldap.Dial("tcp", uri)
+			if err == nil {
+                        	break
+                	}
+		}
+
+		if err != nil {
                         log.Fatal(err)
                 }
+
                 defer l.Close()
 
                 err = l.StartTLS(conf)
@@ -147,6 +138,5 @@ func main() {
                 for _, entry := range sr.Entries {
                         fmt.Printf("%v\n",  entry.GetAttributeValue(pubkey_property))
                 }
-
         }
 }
